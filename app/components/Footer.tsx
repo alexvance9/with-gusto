0a7a21808f1fcd03623230d629ecd35bb86e1d9b
alexvance9 <alexvance9@gmail.com>
2023-10-22 20:00:47 -0700

    basic layout components, header, footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>Website design and development by Alex Vance</div>
+  )
+}
+
+export default Footer
---
+++ app/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <h1>With Gusto!</h1>
+    <p>A food blog, with music. A music blog, with food.</p>
+    </>
+  )
+}
+
+export default Header
